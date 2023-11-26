8e68bee7e24006ff4756f16c1fc0123194e59b57
ndeyerokhayandiaye <rokhayandeye8@gmail.com>
2023-11-26 01:27:49 +0000

    first commit

--- src/app/liste-apprenant/liste-apprenant.component.ts
+++ src/app/liste-apprenant/liste-apprenant.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { AuthentificationService } from '../services/authentification.service';
+import { HttpClient } from '@angular/common/http';
+// import { Personne } from 'test-crud-loopback/src/models';
+
 
 @Component({
   selector: 'app-liste-apprenant',
@@ -7,20 +10,70 @@ import { AuthentificationService } from '../services/authentification.service';
   styleUrls: ['./liste-apprenant.component.css']
 })
 export class ListeApprenantComponent {
+// personnes: any;
+objectPersonne: any;
+listepersonne: any;
+modifierUtilisateur(personnes: any) {
+throw new Error('Method not implemented.');
+}
 
-  data: any [] = [];
+  personnes: any [] = [];
 
-  constructor(private dataService: AuthentificationService) {}
+
+  constructor(private dataService: AuthentificationService,
+     private httpclient : HttpClient ) {}
 
   ngOnInit() {
     this.dataService.getData().subscribe((result) => {
-      this.data = result;
-      console.log()
+      this.personnes = result;
+      console.log(this.personnes)
       // Utilisez les données récupérées comme nécessaire
     });
-  }
-  modifierpersonne(){
     
   }
+
 }
+
+// export class ListeApprenantComponent implements OnInit {
+//   data?: Personne[];
+//   objetAmodifier!: Personne;
+//   personnes!: Personne;
+// // personnes: Personne;
+
+//   constructor(private authService: AuthentificationService) {}
+
+//   ngOnInit(): void {
+//     this.loadData();
+//   }
+
+//   loadData(): void {
+//     this.authService.getData().subscribe(
+//       (data: Personne[]) => {
+//         this.data = data;
+//       },
+//       (error) => {
+//         console.error('Une erreur s\'est produite lors du chargement des données :', error);
+//       }
+//     );
+//   }
+
+//   modifierUtilisateur(personne: Personne): void {
+//     this.objetAmodifier = personne;
+//     // Réinitialiser le formulaire ou effectuer d'autres actions si nécessaire
+//   }
+
+//   validerModification(): void {
+//     if (this.objetAmodifier) {
+//       this.authService.modifierUtilisateur(this.objetAmodifier).subscribe(
+//         () => {
+//           console.log('Utilisateur modifié avec succès.');
+//           // Réinitialiser la variable objetAmodifier et effectuer d'autres actions si nécessaire
+//         },
+//         (error) => {
+//           console.error('Une erreur s\'est produite lors de la modification de l\'utilisateur :', error);
+//         }
+//       );
+//     }
+//   }
+// }
 
--- src/app/services/authentification.service.ts
+++ src/app/services/authentification.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+// import { Personne } from 'test-crud-loopback/src/models';
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,20 @@ export class AuthentificationService {
   getData(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
   }
+
+  // getData(): Observable<Personne[]> {
+  //   return this.http.get<Personne[]>(this.apiUrl);
+  // }
+
+  modifierUtilisateur(personnes: any): Observable<any> {
+    const url = `${this.apiUrl}/${personnes.id}`; // Assurez-vous de remplacer "id" par la propriété d'identification correcte de votre modèle d'utilisateur
+    return this.http.put(url, personnes);
+  }
+
+  // modifierUtilisateur(personne: Personne): Observable<any> {
+  //   const url = `${this.apiUrl}/${personne.id}`;
+  //   return this.http.put(url, personne);
+  // }
+
 }
 
